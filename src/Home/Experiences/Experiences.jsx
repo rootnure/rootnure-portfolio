343015f8e32f0b60df6609870b41a97c8505dc39
rootnure <nur.diu.2791@gmail.com>
2023-12-12 09:42:19 +0600

    Experience card modified

--- src/Home/Experiences/Experiences.jsx
+++ src/Home/Experiences/Experiences.jsx
@@ -2,12 +2,14 @@ import SectionTitle from "../../components/SectionTitle";
 import imgExp1 from "../../assets/photos/ProgrammingHero-b8.png";
 import imgExp2 from "../../assets/photos/master-html-css.jpg";
 import imgExp3 from "../../assets/photos/master-git-and-github.jpg";
+import checkmark from "../../assets/photos/blue-check-mark.png";
 
 const Experiences = () => {
   return (
     <section className="my-12" name="experiences">
       <SectionTitle>Experiences</SectionTitle>
       <div className="space-y-6">
+        {/* Programming Hero (Batch 8) */}
         <div className="flex items-center gap-12 p-4 border rounded-xl group hover:border-orange-500">
           <figure className="w-[500px]">
             <img
@@ -16,7 +18,7 @@ const Experiences = () => {
               className="w-[500px] h-[300px] object-cover rounded-lg group-hover:-rotate-1 group-hover:skew-x-1 group-hover:skew-y-1 group-hover:scale-[95%] duration-150"
             />
           </figure>
-          <div className="space-y-4 flex-1 text-right">
+          <div className="space-y-6 flex-1 text-right">
             <a
               href="https://web.programming-hero.com/home"
               className="hover:underline">
@@ -36,6 +38,7 @@ const Experiences = () => {
             </p>
           </div>
         </div>
+        {/* Udemy HTML and CSS */}
         <div className="flex flex-row-reverse items-center gap-12 p-4 border rounded-xl group hover:border-orange-500">
           <figure className="w-[500px]">
             <img
@@ -44,7 +47,7 @@ const Experiences = () => {
               className="w-[500px] h-[300px] object-cover rounded-lg group-hover:rotate-1 group-hover:-skew-x-1 group-hover:-skew-y-1 group-hover:scale-[95%] duration-150"
             />
           </figure>
-          <div className="space-y-6 flex-1">
+          <div className="space-y-4 flex-1">
             <a
               href="https://www.udemy.com/course/master-html-and-css-by-building-real-world-projects/"
               className="hover:underline">
@@ -57,9 +60,28 @@ const Experiences = () => {
               Learn css, flex, grid, web accessibility, responsiveness, BEM
               architecture, animation by building e-commerce project
             </p>
+            <ul className="text-sm space-y-1">
+              <li className="flex items-center gap-2">
+                <img src={checkmark} alt="" className="h-3" />
+                Design real-world and responsive websites
+              </li>
+              <li className="flex items-center gap-2">
+                <img src={checkmark} alt="" className="h-3" />
+                Basics to advanced HTML including Web accessibility
+              </li>
+              <li className="flex items-center gap-2">
+                <img src={checkmark} alt="" className="h-3" />
+                CSS Advanced Layout including flex, grid layout
+              </li>
+              <li className="flex items-center gap-2">
+                <img src={checkmark} alt="" className="h-3" />
+                Advanced CSS including Specificity calculation, BEM Architecture
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="flex items-center gap-12 p-4 border rounded-xl group hover:border-orange-500">
+        {/* Udemy Git and GitHub */}
+        <div className="flex items-center gap-22 p-4 border rounded-xl group hover:border-orange-500">
           <figure className="w-[500px]">
             <img
               src={imgExp3}
@@ -67,7 +89,7 @@ const Experiences = () => {
               className="w-[500px] h-[300px] object-cover rounded-lg group-hover:-rotate-1 group-hover:skew-x-1 group-hover:skew-y-1 group-hover:scale-[95%] duration-150"
             />
           </figure>
-          <div className="space-y-6 flex-1 text-right">
+          <div className="space-y-4 flex-1 text-right">
             <a
               href="https://www.udemy.com/course/git-and-github-for-absolute-beginners/"
               className="hover:underline">
@@ -80,6 +102,25 @@ const Experiences = () => {
               Learn git configuration, commit, branching, merging, contributing
               and collaboration with other&apos;s project
             </p>
+            <ul className="text-sm flex flex-col items-end space-y-1">
+              <li className="flex items-center gap-2">
+                Git overall work flow including staging, upstaging, committing
+                etc.
+                <img src={checkmark} alt="" className="h-3" />
+              </li>
+              <li className="flex items-center gap-2">
+                How to work with local and remote repositories - push, pull
+                <img src={checkmark} alt="" className="h-3" />
+              </li>
+              <li className="flex items-center gap-2">
+                How to Contribute to other&apos;s project - fork, clone
+                <img src={checkmark} alt="" className="h-3" />
+              </li>
+              <li className="flex items-center gap-2">
+                How to branching in Git and GitHub
+                <img src={checkmark} alt="" className="h-3" />
+              </li>
+            </ul>
           </div>
         </div>
       </div>
